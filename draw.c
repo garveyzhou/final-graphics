4a324a00c7852a849991f8f139f915f00396b685
GarveyZhou <gzhou2@stuy.edu>
2019-06-12 11:48:56 -0400

    added generate cone

--- draw.c
+++ draw.c
@@ -421,7 +421,7 @@ struct matrix * generate_sphere(double cx, double cy, double cz,
 
   should call generate_torus to create the necessary points
   ====================*/
-void add_torus( struct matrix * edges, 
+void add_torus( struct matrix * edges,
                 double cx, double cy, double cz,
                 double r1, double r2, int step ) {
 
@@ -463,6 +463,44 @@ void add_torus( struct matrix * edges,
                    points->m[1][p1],
                    points->m[2][p1]);
     }
+  }
+  free_matrix(points);
+}
+void add_cone(struct matrix * polygons, double cx, double cy, double cz,double r1, double h, int step ) {
+  struct matrix * points = new_matrix(4,step * step);
+  int circle, rot_start, rot_stop;
+  add_point(points,cx,cy,cz+h);
+  double x,y,z,rot,circ;
+  rot_start = 0;
+  rot_stop = step;
+  x = cx + r;
+  y = cy;
+  z = cz;
+  add_point(points,x,y,z);
+  for(int i = rot_start; i <rot_stop; i ++){
+    circ = (double) i /step;
+    y = r * sin(2 * M_PI * circ) + cy;
+    x = r * cos(2 * M_PI * circ) + cx;
+    add_point(points,x,y,z);
+  }
+  x0 = points->m[0][0];
+  y0 = points->m[1][0];
+  z0 = points->m[2][0];
+  for(int i = 1; i < step;i++){
+    x1 = points->m[0][i];
+    y1 = points->m[1][i];
+    z1 = points->m[2][i];
+    if(i == step - 1){
+      x2 = points->m[0][1];
+      y2 = points->m[1][1];
+      z2 = points->m[2][1];
+    }
+    else{
+      x2 = points->m[0][i+1];
+      y2 = points->m[0][i+1];
+      z2 = points->m[0][i+1];
+    }
+    add_polygon(polygons,x0,y0,z0,x1,y1,z1,x2,y2,z2);
   }
   free_matrix(points);
 }
@@ -559,10 +597,10 @@ of type specified in type (see matrix.h for curve type constants)
 to the matrix edges
 ====================*/
 void add_curve( struct matrix *edges,
-                double x0, double y0, 
-                double x1, double y1, 
-                double x2, double y2, 
-                double x3, double y3, 
+                double x0, double y0,
+                double x1, double y1,
+                double x2, double y2,
+                double x3, double y3,
                 int step, int type ) {
   double t, x, y;
   int i;
@@ -571,11 +609,11 @@ void add_curve( struct matrix *edges,
 
   xcoefs = generate_curve_coefs(x0, x1, x2, x3, type);
   ycoefs = generate_curve_coefs(y0, y1, y2, y3, type);
-  
+
   /* print_matrix(xcoefs); */
   /* printf("\n"); */
   /* print_matrix(ycoefs); */
-  
+
   for (i=1; i<=step; i++) {
     t = (double)i/step;
 
@@ -599,8 +637,8 @@ void add_curve( struct matrix *edges,
 Inputs:   struct matrix * points
          int x
          int y
-         int z 
-Returns: 
+         int z
+Returns:
 adds point (x, y, z) to points and increment points.lastcol
 if points is full, should call grow on points
 ====================*/
@@ -608,7 +646,7 @@ void add_point( struct matrix * points, double x, double y, double z) {
 
   if ( points->lastcol == points->cols )
     grow_matrix( points, points->lastcol + 100 );
-  
+
   points->m[0][ points->lastcol ] = x;
   points->m[1][ points->lastcol ] = y;
   points->m[2][ points->lastcol ] = z;
@@ -619,12 +657,12 @@ void add_point( struct matrix * points, double x, double y, double z) {
 /*======== void add_edge() ==========
 Inputs:   struct matrix * points
           int x0, int y0, int z0, int x1, int y1, int z1
-Returns: 
+Returns:
 add the line connecting (x0, y0, z0) to (x1, y1, z1) to points
 should use add_point
 ====================*/
-void add_edge( struct matrix * points, 
-	       double x0, double y0, double z0, 
+void add_edge( struct matrix * points,
+	       double x0, double y0, double z0,
 	       double x1, double y1, double z1) {
   add_point( points, x0, y0, z0 );
   add_point( points, x1, y1, z1 );
@@ -633,8 +671,8 @@ void add_edge( struct matrix * points,
 /*======== void draw_lines() ==========
 Inputs:   struct matrix * points
          screen s
-         color c 
-Returns: 
+         color c
+Returns:
 Go through points 2 at a time and call draw_line to add that line
 to the screen
 ====================*/
--- mdl.y
+++ mdl.y
@@ -103,7 +103,63 @@ SPHERE STRING DOUBLE DOUBLE DOUBLE DOUBLE STRING
   op[lastop].op.sphere.constants = add_symbol($2,SYM_CONSTANTS,c);
   lastop++;
 }|
-
+SPHERE DOUBLE DOUBLE DOUBLE DOUBLE
+{
+  lineno++;
+  op[lastop].opcode = SPHERE;
+  op[lastop].op.sphere.d[0] = $2;
+  op[lastop].op.sphere.d[1] = $3;
+  op[lastop].op.sphere.d[2] = $4;
+  op[lastop].op.sphere.d[3] = 0;
+  op[lastop].op.sphere.r = $5;
+  op[lastop].op.sphere.constants = NULL;
+  op[lastop].op.sphere.cs = NULL;
+  lastop++;
+}|
+SPHERE DOUBLE DOUBLE DOUBLE DOUBLE STRING
+{
+  lineno++;
+  op[lastop].opcode = SPHERE;
+  op[lastop].op.sphere.d[0] = $2;
+  op[lastop].op.sphere.d[1] = $3;
+  op[lastop].op.sphere.d[2] = $4;
+  op[lastop].op.sphere.d[3] = 0;
+  op[lastop].op.sphere.r = $5;
+  op[lastop].op.sphere.constants = NULL;
+  m = (struct matrix *)new_matrix(4,4);
+  op[lastop].op.sphere.cs = add_symbol($6,SYM_MATRIX,m);
+  lastop++;
+}|
+SPHERE STRING DOUBLE DOUBLE DOUBLE DOUBLE
+{
+  lineno++;
+  op[lastop].opcode = SPHERE;
+  op[lastop].op.sphere.d[0] = $3;
+  op[lastop].op.sphere.d[1] = $4;
+  op[lastop].op.sphere.d[2] = $5;
+  op[lastop].op.sphere.d[3] = 0;
+  op[lastop].op.sphere.r = $6;
+  op[lastop].op.sphere.cs = NULL;
+  c = (struct constants *)malloc(sizeof(struct constants));
+  op[lastop].op.sphere.constants = add_symbol($2,SYM_CONSTANTS,c);
+  lastop++;
+}|
+SPHERE STRING DOUBLE DOUBLE DOUBLE DOUBLE STRING
+{
+  lineno++;
+  op[lastop].opcode = SPHERE;
+  op[lastop].op.sphere.d[0] = $3;
+  op[lastop].op.sphere.d[1] = $4;
+  op[lastop].op.sphere.d[2] = $5;
+  op[lastop].op.sphere.d[3] = 0;
+  op[lastop].op.sphere.r = $6;
+  op[lastop].op.sphere.constants = NULL;
+  m = (struct matrix *)new_matrix(4,4);
+  op[lastop].op.sphere.cs = add_symbol($7,SYM_MATRIX,m);
+  c = (struct constants *)malloc(sizeof(struct constants));
+  op[lastop].op.sphere.constants = add_symbol($2,SYM_CONSTANTS,c);
+  lastop++;
+}|
 TORUS DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE
 {
   lineno++;
@@ -167,7 +223,69 @@ TORUS STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING
 
   lastop++;
 }|
+CONE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE
+{
+  lineno++;
+  op[lastop].opcode = CONE;
+  op[lastop].op.cone.d[0] = $2;
+  op[lastop].op.cone.d[1] = $3;
+  op[lastop].op.cone.d[2] = $4;
+  op[lastop].op.cone.d[3] = 0;
+  op[lastop].op.cone.r0 = $5;
+  op[lastop].op.cone.r1 = $6;
+  op[lastop].op.cone.constants = NULL;
+  op[lastop].op.cone.cs = NULL;
 
+  lastop++;
+}|
+CONE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING
+{
+  lineno++;
+  op[lastop].opcode = CONE;
+  op[lastop].op.cone.d[0] = $2;
+  op[lastop].op.cone.d[1] = $3;
+  op[lastop].op.cone.d[2] = $4;
+  op[lastop].op.cone.d[3] = 0;
+  op[lastop].op.cone.r0 = $5;
+  op[lastop].op.cone.r1 = $6;
+  op[lastop].op.cone.constants = NULL;
+  m = (struct matrix *)new_matrix(4,4);
+  op[lastop].op.cone.cs = add_symbol($7,SYM_MATRIX,m);
+  lastop++;
+}|
+CONE STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE
+{
+  lineno++;
+  op[lastop].opcode = CONE;
+  op[lastop].op.cone.d[0] = $3;
+  op[lastop].op.cone.d[1] = $4;
+  op[lastop].op.cone.d[2] = $5;
+  op[lastop].op.cone.d[3] = 0;
+  op[lastop].op.cone.r0 = $6;
+  op[lastop].op.cone.r1 = $7;
+  op[lastop].op.cone.cs = NULL;
+  c = (struct constants *)malloc(sizeof(struct constants));
+  op[lastop].op.cone.constants = add_symbol($2,SYM_CONSTANTS,c);
+
+  lastop++;
+}|
+CONE STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING
+{
+  lineno++;
+  op[lastop].opcode = CONE;
+  op[lastop].op.cone.d[0] = $3;
+  op[lastop].op.cone.d[1] = $4;
+  op[lastop].op.cone.d[2] = $5;
+  op[lastop].op.cone.d[3] = 0;
+  op[lastop].op.cone.r0 = $6;
+  op[lastop].op.cone.r1 = $7;
+  c = (struct constants *)malloc(sizeof(struct constants));
+  op[lastop].op.cone.constants = add_symbol($2,SYM_CONSTANTS,c);
+  m = (struct matrix *)new_matrix(4,4);
+  op[lastop].op.cone.cs = add_symbol($8,SYM_MATRIX,m);
+
+  lastop++;
+}|
 BOX DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE
 {
   lineno++;
